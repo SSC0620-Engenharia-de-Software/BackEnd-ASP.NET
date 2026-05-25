367c31b8504bc30d1ddebb41a4108ee12aa57bcf
LuisFSForti <luisforti@usp.br>
2026-05-24 22:52:18 -0300

    Mudança CORS
    Adição de CORS para o endereço localhost:3000, para que o react tenha acesso.

--- src/Program.cs
+++ src/Program.cs
@@ -2,12 +2,29 @@ using src.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers();
+
+//CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy
+            .WithOrigins("http://localhost:3000")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddSingleton<TesteConexao>();
 
 var app = builder.Build();
+
+// IMPORTANTE: UseCors ANTES dos endpoints
+app.UseCors("AllowFrontend");
 
 //if (app.Environment.IsDevelopment())
 {
@@ -21,5 +38,7 @@ app.MapGet("/Teste", async (TesteConexao db) =>
 {
     return await db.PegarPessoas();
 });
+
+app.MapControllers();
 
 app.Run();
